0dae2e4127d82c42ef3350b0c6627911a9daadd3
Rudransh Shinghal <84223519+Ansh1902396@users.noreply.github.com>
2024-12-16 05:34:34 +0530

    Update courseGenerator.ts

--- src/lib/courseGenerator.ts
+++ src/lib/courseGenerator.ts
@@ -5,11 +5,7 @@ import { createOpenAI } from '@ai-sdk/openai';
 
 
 
-const openai = createOpenAI({
-    // custom settings, e.g.
-    apiKey  : process.env.OPENAI_API_KEY,
-    compatibility: 'strict', // strict mode, enable when using the OpenAI API
-  });
+
 
 
   const vertexAI = createVertex({
